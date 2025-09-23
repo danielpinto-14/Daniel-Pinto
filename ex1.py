5ac3f1ee52cf8c67019db2bcfcbf54882dcc2a5b
danielpinto-14 <danielvalepinto@escultorfsa.pt>
2025-09-23 19:56:05 +0100

    Add files via upload
    Exercício em que aprendi a calcular variáveis usando o Python.

---
+++ ex1.py
@@ -0,0 +1,32 @@
+#Pedir o nome do Aluno:
+
+sobrenome = input ("Qual o seu sobrenome: ")
+nome = "Vamos fazer cálculos :)"
+print("Olá Sharks: ", nome, sobrenome) 
+
+#Soma de dois números:
+numero1 = 5
+numero2 = 10
+soma = numero1 + numero2
+print("a soma dos números é de: ", soma)
+
+#Divisão de dois números:
+numero3 = 10
+numero4 = 5
+divisao = numero3 / numero4
+print("a divisão dos números é de: ", divisao)
+
+#Multiplicação de dois números:
+numero5 = 50
+numero6 = 70
+multiplicacao = numero5 * numero6
+print("a multiplicacao dos números é de: ", multiplicacao)
+
+#Média de cinco números:
+nota1 = 4
+nota2 = 38
+nota3 = 15
+nota4 = 172
+nota5 = 49
+media = (nota1 + nota2 + nota3 + nota4 + nota5) / 5
+print("a média dos cinco números é de: ", media)
