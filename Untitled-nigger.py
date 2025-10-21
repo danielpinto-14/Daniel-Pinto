1dbbb3fb4527b4719b8e7c1df9af97b82a1874dd
danielpinto-14 <danielvalepinto@escultorfsa.pt>
2025-10-21 19:55:58 +0100

    Add files via upload

---
+++ Untitled-nigger.py
@@ -0,0 +1,12 @@
+#tdxctyio: 
+nota1 = 86
+nota2 = 58
+nota3 = 4
+nota4 = 90
+nota5 = 67
+media = (nota1 + nota2 + nota3 + nota4 + nota5) / 5
+if media>60:
+    print("Parabéns! Aluno aprovado")
+else:
+    print("Aluno Reprovado")
+
