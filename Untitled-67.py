51558eb240d6cd62313d57d1364fad26d9d58ab5
danielpinto-14 <danielvalepinto@escultorfsa.pt>
2025-10-21 19:57:48 +0100

    Add files via upload

---
+++ Untitled-67.py
@@ -0,0 +1,9 @@
+idade_carro = int(input("Quatos anos tem o seu veículo?: "))
+if idade_carro<10:
+    print("O seu carro é seminovo")
+elif idade_carro<2:
+    print("O seu carro Novo!!")
+else:
+    print("O seu carro é antigo...")
+
+    
